f192624f32768e3cd74c56d7563d60e2418c627f
Dheeraj Sahu <dheerajsahu9943@gmail.com>
2024-06-22 17:36:15 +0530

    update

--- calc/views.py
+++ calc/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html',{'name':'Kiran'})
+    return render(request,'home.html',{'name':'Dheeraj'})
+
+
+def add(request):
+
+    val1=int(request.POST['num1'])
+    val2=int(request.POST['num2'])
+    res=val1+val2
+    return render(request,'result.html',{'result':res})
